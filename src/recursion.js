bb8c7d57d1730753ced3bc3d864d0b24e5074517
Adam Tiller <a.j.tiller@gmail.com>
2020-03-02 14:09:46 -0500

    Add minimizeZeroes

--- src/recursion.js
+++ src/recursion.js
@@ -590,6 +590,15 @@ var augmentElements = function(array, aug) {
 // minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
 // minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
 var minimizeZeroes = function(array) {
+	if (array.length === 0) {
+		return [];
+	} else {
+		let arr = minimizeZeroes(array.slice(0,array.length - 1));
+		if ((array[array.length - 1] !== arr[arr.length - 1]) || (array[array.length - 1] !== 0)) {
+			arr.push(array[array.length - 1]);
+		}
+		return arr;
+	}
 };
 
 // 35. Alternate the numbers in an array between positive and negative regardless of
