2893864813a15762c75fc5192f000c57eeb2b6da
Adam Tiller <a.j.tiller@gmail.com>
2020-03-02 13:52:53 -0500

    Add compress

--- src/recursion.js
+++ src/recursion.js
@@ -560,6 +560,16 @@ var letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 var compress = function(list) {
+
+	if (list.length === 0) {
+		return [];
+	} else {
+		let arr = compress(list.slice(0,list.length - 1));
+		if (list[list.length - 1] !== arr[arr.length - 1]) {
+			arr.push(list[list.length - 1]);
+		}
+		return arr;
+	}
 };
 
 // 33. Augment every element in a list with a new value where each element is an array
