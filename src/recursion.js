12ad20564a2d07e9ab48f23c1ca633dead591c9e
Adam Tiller <a.j.tiller@gmail.com>
2020-03-01 20:48:42 -0500

    Add fibonacci and nthFib

--- src/recursion.js
+++ src/recursion.js
@@ -428,7 +428,6 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 	}
 
 	return obj;
-
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
@@ -437,6 +436,16 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+	
+	if (n === 1) {
+		return [0,1];
+	} else if (n > 1 ){
+		let arr = fibonacci(n - 1);
+		arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+		return arr;
+	} else {
+		return null;
+	}
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
@@ -445,6 +454,15 @@ var fibonacci = function(n) {
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
+	if (n === 0) {
+		return 0;
+	} else if (n === 1) {
+		return 1;
+	} else if (n > 1) {
+		return nthFibo(n - 1) + nthFibo(n - 2);
+	} else {
+		return null;
+	}
 };
 
 // 27. Given an array of words, return a new array containing each word capitalized.
